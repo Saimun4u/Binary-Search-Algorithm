1f8cf000e3f5547b015af2a5c76e56ee790ce7be
Saimun Hassan <saimun4u@gmail.com>
2023-05-20 15:15:54 -0400

    Setup of function, testing with lists

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def binary_search(list, element):
     steps = 0
 
     while start<=end:
-      print(f'"Steps",steps, ":", str(list[start:end+1])')
+      print(f'Step {steps} : {str(list[start:end+1])}')
 
       steps = steps + 1
       middle = (start + end) // 2
@@ -20,4 +20,11 @@ def binary_search(list, element):
       if element < list[middle]:
          end = middle - 1
       else:
-         start = middle + 1   
+         start = middle + 1
+
+    return -1
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+target = 12
+
+binary_search(my_list, target)
